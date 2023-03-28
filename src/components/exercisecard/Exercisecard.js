e71263624cd05734985f4063b0b81768639e1fcd
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-03-28 01:40:54 +0100

    add ability to start/pause gif on hover

--- src/components/exercisecard/Exercisecard.js
+++ src/components/exercisecard/Exercisecard.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import GifModal from '../modal/GifModal.js'
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import GifModal from "../modal/GifModal.js";
 // import avocatio from '../../assets/images/avocatio.jpg';
+import ReactFreezeframe from "react-freezeframe";
 
 // nice to have - skeleton
 // import Box from '@mui/material/Box';
@@ -14,45 +15,52 @@ import GifModal from '../modal/GifModal.js'
 // import Stack from '@mui/material/Stack';
 
 export default function Exercisecard(props) {
-
   const [open, setOpen] = useState(false);
 
   const handleOpen = (e) => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
-    <Card 
-    sx={{ display: 'flex', flexDirection: 'column', maxHeight: '600px', maxWidth: '400px', marginBottom: '10px',  minWidth: '300px', minHeight: '560px', }}
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxHeight: "600px",
+        maxWidth: "400px",
+        marginBottom: "10px",
+        minWidth: "300px",
+        minHeight: "560px",
+      }}
     >
       <CardMedia
-        sx={{ height:'380px'}}
-        component="img"
-        image={props.gifUrl}
+        // sx={{ height: "380px" }}
+        // component="img"
+        // image={props.gifUrl}
         title={props.name}
+        key={props.id}
       />
-      <CardContent
-        sx={{ height:'90px'}}
-        >
+      <ReactFreezeframe src={props.gifUrl} />
+      <CardContent sx={{ height: "90px" }}>
         <Typography gutterBottom variant="h5" component="div">
-        {props.name}
+          {props.name}
         </Typography>
 
         {/* <Typography variant="body2" color="text.secondary">
           Short exercise description if we like to 
         </Typography> */}
-
       </CardContent>
-
       <CardActions>
         {/*<Button size="small">Share</Button> */}
-        <Button size="small" cursor="pointer" onClick={handleOpen}>Learn More</Button>
-        <GifModal 
-        open={open} 
-        handleClose={handleClose} 
-        image={props.gifUrl}
-        title={props.name}
-        equipment={props.equipment}
-        target={props.target}
+        <Button size="small" cursor="pointer" onClick={handleOpen}>
+          Learn More
+        </Button>
+        <GifModal
+          open={open}
+          handleClose={handleClose}
+          image={props.gifUrl}
+          title={props.name}
+          equipment={props.equipment}
+          target={props.target}
         />
       </CardActions>
     </Card>
