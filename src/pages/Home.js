f49114d8cfe82268fc82841213dbd1a495c0f395
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-03-28 19:11:49 +0100

    add id as key props in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,75 +1,118 @@
-import React, { useState, useEffect } from 'react'
-import Searchbar from '../components/searchbar/Searchbar'
-import Exercisecard from '../components/exercisecard/Exercisecard'
-import Grid from '@mui/material/Grid'
-import API from '../utils/ExerciseDB.js'
-
+import React, { useState, useEffect } from "react";
+import Searchbar from "../components/searchbar/Searchbar";
+import Exercisecard from "../components/exercisecard/Exercisecard";
+import Grid from "@mui/material/Grid";
+import API from "../utils/ExerciseDB.js";
 
 // body parts options
 const bodyParts = [
-  "back", "cardio", "chest", "lower arms", "lower legs", "neck", "shoulders", "upper arms", "upper legs", "waist"
-    ]
+  "back",
+  "cardio",
+  "chest",
+  "lower arms",
+  "lower legs",
+  "neck",
+  "shoulders",
+  "upper arms",
+  "upper legs",
+  "waist",
+];
 
-const bodyPartsObj = bodyParts.map((bodyPart) => { 
+const bodyPartsObj = bodyParts.map((bodyPart) => {
   return {
-    type: "Body parts", 
-    name: bodyPart
-    };
-  })
+    type: "Body parts",
+    name: bodyPart,
+  };
+});
 
 // equipments options
 const equipments = [
-  "assisted", "band", "barbell", "body weight", "bosu ball", "cable", "dumbbell", "elliptical machine",
-  "ez barbell", "hammer", "kettlebell", "leverage machine", "medicine ball", "olympic barbell", "resistance band", 
-  "roller", "rope", "skierg machine", "sled machine", "smith machine", "stability ball", "stationary bike",
-  "stepmill machine", "tire", "trap bar", "upper body ergometer", "weighted", "wheel roller"
-  ]
+  "assisted",
+  "band",
+  "barbell",
+  "body weight",
+  "bosu ball",
+  "cable",
+  "dumbbell",
+  "elliptical machine",
+  "ez barbell",
+  "hammer",
+  "kettlebell",
+  "leverage machine",
+  "medicine ball",
+  "olympic barbell",
+  "resistance band",
+  "roller",
+  "rope",
+  "skierg machine",
+  "sled machine",
+  "smith machine",
+  "stability ball",
+  "stationary bike",
+  "stepmill machine",
+  "tire",
+  "trap bar",
+  "upper body ergometer",
+  "weighted",
+  "wheel roller",
+];
 
-const equipmentsObj = equipments.map((equipment) => { 
+const equipmentsObj = equipments.map((equipment) => {
   return {
-    type: "Equipments", 
-    name: equipment
-    };
-  })  
+    type: "Equipments",
+    name: equipment,
+  };
+});
 
 // muscles options
-const muscles = [ 
-  "abductors", "abs", "adductors", "biceps", "calves", "cardiovascular system", "delts", "forearms", "glutes",
-  "hamstrings", "lats", "levator scapulae", "pectorals", "quads", "serratus anterior", "spine", "traps", "triceps",
-  "upper back"
-]    
+const muscles = [
+  "abductors",
+  "abs",
+  "adductors",
+  "biceps",
+  "calves",
+  "cardiovascular system",
+  "delts",
+  "forearms",
+  "glutes",
+  "hamstrings",
+  "lats",
+  "levator scapulae",
+  "pectorals",
+  "quads",
+  "serratus anterior",
+  "spine",
+  "traps",
+  "triceps",
+  "upper back",
+];
 
-const musclesObj = muscles.map((muscle) => { 
+const musclesObj = muscles.map((muscle) => {
   return {
-    type: "Target muscle", 
-    name: muscle
-    };
-  })  
+    type: "Target muscle",
+    name: muscle,
+  };
+});
 
-// concatenate content of all objects 
-const options = [...bodyPartsObj, ...equipmentsObj, ...musclesObj]
-
+// concatenate content of all objects
+const options = [...bodyPartsObj, ...equipmentsObj, ...musclesObj];
 
 function Home() {
-
   const [exercises, setExercises] = useState([]);
   const [inputField, setInputField] = useState();
-  
+
   useEffect(() => {
-  
     // if there is a searched item
     if (inputField !== undefined) {
+      // const search = inputField.replace(" ", "%");
 
-      // const search = inputField.replace(" ", "%"); 
-
-      // stores the API call function taking the endpoint name as argument 
+      // stores the API call function taking the endpoint name as argument
       const APICall = (endpoint) => {
-
         API.getExercises(`${endpoint}=${inputField}`).then((res) => {
-          setExercises(res); 
+          setExercises(res);
         });
-      }
-      
+      };
+
       // if the item searched is part of the body parts list
       if (bodyParts.includes(inputField)) {
         // uses the body parts endpoint
@@ -78,64 +121,61 @@ function Home() {
 
       // if the item searched is part of the body parts list
       else if (equipments.includes(inputField)) {
-      // uses the equipment endpoint
+        // uses the equipment endpoint
         APICall("equipment");
       }
 
       // if the item searched is part of the muscle list
       else if (muscles.includes(inputField)) {
-      // uses the target muscle endpoint
+        // uses the target muscle endpoint
         APICall("target");
       }
-    }  
-  }
-  , [inputField]);
+    }
+  }, [inputField]);
 
   // update input field state when the search button is clicked
   const handleClick = () => {
-    setInputField(document.getElementById("combo-box").value); 
-  }
+    setInputField(document.getElementById("combo-box").value);
+  };
 
   // update input field state when the enter key is pressed
   const handleEnter = (e) => {
     // if the enter key is pressed
     if (e.key === "enter" || e.keyCode === 13) {
-      setInputField(document.getElementById("combo-box").value); 
+      setInputField(document.getElementById("combo-box").value);
     }
-  }
-
-
-
+  };
 
   return (
     <div>
-        <Searchbar 
-          handleClick={handleClick}
-          handleEnter={handleEnter} 
-          options={options}
-        />
-        
-        <Grid container  wrap="nowrap"  columnGap={'2%'} rowGap={'2%'}
-        sx = {{ flexDirection: {lg:'row', md:'row', sm:'column', xs:'column'},
+      <Searchbar
+        handleClick={handleClick}
+        handleEnter={handleEnter}
+        options={options}
+      />
 
+      <Grid
+        container
+        wrap="nowrap"
+        columnGap={"2%"}
+        rowGap={"2%"}
+        sx={{
+          flexDirection: { lg: "row", md: "row", sm: "column", xs: "column" },
         }}
-            style={{
-                minHeight: '600px',
-                display: 'flex',
-                alignItems: 'center', 
-                justifyContent: 'center',   
-                padding: '1rem',   
-         }}>
-              
-          {exercises.map(exercise => (
-            <Exercisecard {...exercise} />
-            ))
-          }
-
-        </Grid>
-
+        style={{
+          minHeight: "600px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "1rem",
+        }}
+      >
+        {exercises.map((exercise) => (
+          <Exercisecard {...exercise} key={exercise.id} />
+        ))}
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
